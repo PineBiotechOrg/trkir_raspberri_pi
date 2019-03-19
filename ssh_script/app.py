15b2c83e94df1849d769ad5d33afbfb9b4fb4148
Novosad Victor <novosad_msu@mail.ru>
2019-03-19 17:35:46 +0300

    convert

--- ssh_script/app.py
+++ ssh_script/app.py
@@ -34,7 +34,7 @@ while True:
             print('Cannot create dir ', TO_PATH)
             sys.exit(1)
         while True:
-            subprocess.run(CONVERT_SCRIPT)
+            subprocess.call(CONVERT_SCRIPT, shell=True)
             sftp_client.put(IMAGE_PATH, os.path.join(TO_PATH, IMG_NAME))
             sftp_client.put(TEMP_PATH, os.path.join(TO_PATH, TEMP_NAME))
             print('putted')
