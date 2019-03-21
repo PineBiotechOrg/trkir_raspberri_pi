a5a5bb91d75c250cca026e61ec076571a30acaa5
Novosad Victor <novosad_msu@mail.ru>
2019-03-21 20:51:16 +0300

    mac address

--- ssh_script/app.py
+++ ssh_script/app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import traceback
 import sys
 import uuid
@@ -37,5 +38,5 @@ while True:
             subprocess.call(CONVERT_SCRIPT, shell=True)
             sftp_client.put(IMAGE_PATH, os.path.join(TO_PATH, IMG_NAME))
             sftp_client.put(TEMP_PATH, os.path.join(TO_PATH, TEMP_NAME))
-            print('putted')
+            print(datetime.now())
             time.sleep(0.1)
