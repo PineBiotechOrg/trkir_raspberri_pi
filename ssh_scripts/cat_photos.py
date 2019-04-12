2f2dadc2d256e9afc75ac2720388f2cab6fb6322
Novosad Victor <novosad_msu@mail.ru>
2019-04-12 19:35:24 +0200

    cat images

--- ssh_scripts/cat_photos.py
+++ ssh_scripts/cat_photos.py
@@ -1,15 +1,19 @@
 from PIL import Image
 import datetime
+import shutil
 import time
-import traceback
+import sys
 
 IMG_NAME = '1.jpg'
+TEMP_NAME = 'temp.txt'
 CORE_PATH = '/storage/pi/'
 
 
 def cut_image(mac_address):
     img_path = CORE_PATH + '{}/{}'.format(mac_address, IMG_NAME)
+    temp_path = CORE_PATH + '{}/{}'.format(mac_address, TEMP_NAME)
     save_path = CORE_PATH + '{}/' + '{}'.format(IMG_NAME)
+    temp_save_path = CORE_PATH + '{}/'
 
     while True:
         try:
@@ -19,11 +23,16 @@ def cut_image(mac_address):
             for i in range(4):
                 if i == 2:
                     j = 1
-                cur_img = im.crop((320 * i, 240 * j, 320 * (i + 1), 240 * (j + 1)))
+                cur_img = im.crop((320 * (i % 2), 240 * j, 320 * ((i % 2) + 1), 240 * (j + 1)))
                 cur_img.thumbnail((160, 120))
                 cur_img.save(save_path.format(mac_address + '_' + str(i)))
+                shutil.copy(temp_path, temp_save_path.format(mac_address + '_' + str(i)))
         except:
-            traceback.print_exc()
+            time.sleep(0.15)
             continue
-        time.sleep(0.1)
+        time.sleep(0.15)
         print(datetime.datetime.now())
+
+
+if __name__ == '__main__':
+    cut_image(sys.argv[1])
